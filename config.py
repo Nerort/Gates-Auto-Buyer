00ae18dd930f83f04513f8a67a1dffff84228899
Nerort <59502452+Nerort@users.noreply.github.com>
2025-06-07 15:16:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 def load_config():
     return {
-        "bot_token": '7309807688:AAH88kamoPeIC8c1WIE6qYd8b4H1FzVyWfs',
+        "bot_token": '',
         "DATABASE_URL": 'sqlite:///user_data.db'
     }
